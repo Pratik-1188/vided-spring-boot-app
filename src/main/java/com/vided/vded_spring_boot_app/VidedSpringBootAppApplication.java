17875119469a6ccbefa62331b9eba4c0157c7dc0
Pratik Bhosale <pratikbhosale1188@gmail.com>
2024-09-08 11:41:55 +0530

    renamed project for main

--- src/main/java/com/vided/vded_spring_boot_app/VidedSpringBootAppApplication.java
+++ src/main/java/com/vided/vded_spring_boot_app/VidedSpringBootAppApplication.java
@@ -1,15 +1,13 @@
 package com.vided.vded_spring_boot_app;
 
-import org.bytedeco.opencv.global.opencv_videoio;
-import org.bytedeco.opencv.opencv_videoio.VideoWriter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class VdedSpringBootAppApplication {
+public class VidedSpringBootAppApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(VdedSpringBootAppApplication.class, args);
+		SpringApplication.run(VidedSpringBootAppApplication.class, args);
 
 	}
 
