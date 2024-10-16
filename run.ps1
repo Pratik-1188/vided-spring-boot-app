e121e29b625a8ea82f15b2243dcd3806cd4e7dbb
Pratik Bhosale <pratikbhosale1188@gmail.com>
2024-10-16 21:46:35 +0530

    - removed mount while running docker container

--- run.ps1
+++ run.ps1
@@ -2,4 +2,5 @@
 
 docker build -t pratik1188/vided-spring-boot-app:v1 .
 docker image prune --force
-docker run -it --rm --name vided-spring-container -v D:/Docker_Mount/vided-root-mount:/mnt -p 8080:8080 pratik1188/vided-spring-boot-app:v1
+#docker run -it --rm --name vided-spring-container -v D:/Docker_Mount/vided-root-mount:/mnt -p 8080:8080 pratik1188/vided-spring-boot-app:v1
+docker run -it --rm --name vided-spring-container -p 8080:8080 pratik1188/vided-spring-boot-app:v1
